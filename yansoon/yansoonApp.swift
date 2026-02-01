6f376917b805cf0ff75968e3e6686485a8c5caf7
Sarah <sara.alwateed17@gmail.com>
2026-02-01 16:55:48 +0300

    Initial Commit

---
+++ yansoon/yansoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  yansoonApp.swift
+//  yansoon
+//
+//  Created by Sarah on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct yansoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

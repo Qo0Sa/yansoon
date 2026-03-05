a8075211398e09db6dc9497fa4326fa7f1b8882f
Rana <rana.k.alngashy@gmail.com>
2026-03-05 06:34:33 +0300

    updated splash screen

--- FontsApp/yansoon/Views/SplashView.swift
+++ FontsApp/yansoon/Views/SplashView.swift
@@ -12,7 +12,6 @@ struct SplashView: View {
 
     @State private var logoScale: CGFloat = 0.85
     @State private var logoOpacity: Double = 0.0
-    @State private var rotation: Angle = .degrees(0)
     @State private var pulse = false
 
     init(durationSeconds: Double = 2.0, onFinish: @escaping () -> Void) {
@@ -24,20 +23,16 @@ struct SplashView: View {
         ZStack {
             Color("Background").ignoresSafeArea()
 
-            Image("yansoonStatus/low")
+            Image("yansoonStatus/high")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 180, height: 180)
                 .scaleEffect(logoScale * (pulse ? 1.03 : 0.97))
-                .rotationEffect(rotation)
                 .opacity(logoOpacity)
                 .onAppear {
                     withAnimation(.easeOut(duration: 0.9)) {
                         logoOpacity = 1.0
                         logoScale = 1.0
-                    }
-                    withAnimation(.easeInOut(duration: 1.2)) {
-                        rotation = .degrees(360)
                     }
                     withAnimation(.easeInOut(duration: 0.9).repeatForever(autoreverses: true)) {
                         pulse = true
@@ -50,4 +45,3 @@ struct SplashView: View {
         }
     }
 }
-
